c270140c1e0dfa297565566c055da06a6f1cc902
Kai Huang <31801496+HelloKevin07@users.noreply.github.com>
2023-05-27 15:27:11 -0400

    Create requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,2 @@
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip3 install transformers==4.28.1 datasets==2.11.0 tqdm==4.64.1 evaluate==0.4.0 peft==0.3.0 matplotlib==3.6.3 nltk rouge_score py7zr
